f0a2d46f2d47891c79d95574b6220bb51daca212
douglasalves <douglas.estefanio@gmail.com>
2022-11-01 17:04:16 -0300

    feat: create route login

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { handleError } from "./middlewares";
+import sessionRoutes from "./routes/sessions.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(handleError);
+
+app.use("/sessions", sessionRoutes);
 
 export default app;
