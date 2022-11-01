121c25a8ad28287c112c223a81cc4e86d691c985
Marcos Gaius <gaius@Coda.localdomain>
2022-11-01 12:19:05 -0400

    fix: fixing entities and env variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,7 +15,7 @@ export const AppDataSource = new DataSource(
         port: process.env.PGPORT ? +process.env.PGPORT : 5432,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        database: process.env.DB,
+        database: process.env.POSTGRES_DB,
         synchronize: false,
         logging: true,
         entities: ["src/entity/*.ts"],
