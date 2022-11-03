3f4462e3135da3687442400ba195ace660c0167d
Vitor Brasil <vitorlbrasil@hotmail.com>
2022-11-03 13:18:28 -0300

    feat: adding veiculos routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { handleError } from "./middlewares";
+import veiculosRouter from "./routes/veiculos.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(handleError);
+app.use("/veiculos", veiculosRouter);
 
 export default app;
