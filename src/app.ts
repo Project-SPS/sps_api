08931f731f89d008c5b7a35e336e54ccc3833627
douglasalves <douglas.estefanio@gmail.com>
2022-11-03 14:40:06 -0300

    feat: create police routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { handleError } from "./middlewares";
+import policeRoutes from "./routes/policiais.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(handleError);
+
+app.use("/policiais", policeRoutes);
 
 export default app;
