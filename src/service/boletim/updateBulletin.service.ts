5f1dd22fc933e326cddeb7703e3a6dfa77f45618
Vitor Brasil <vitorlbrasil@hotmail.com>
2022-11-04 15:40:07 -0300

    fix: refactoring boletins controllers

--- src/service/boletim/updateBulletin.service.ts
+++ src/service/boletim/updateBulletin.service.ts
@@ -4,17 +4,19 @@ import { AppDataSource } from "../../data-source";
 import { updateBulletinSerializer } from "../../serializer/boletim.serializer";
 import { IBoletimUpdateRequest } from "../../interfaces/boletim.interfaces";
 
+const updateBulletinService = async (
+  data: IBoletimUpdateRequest,
+  id: string
+): Promise<Boletim> => {
+  const updateSerializer = await updateBulletinSerializer.validate(data);
+  const boletimRepository = AppDataSource.getRepository(Boletim);
+  const findBoletim = await boletimRepository.findOneBy({ id });
+  if (!findBoletim) {
+    throw new AppError("Boletim de ocorrencia não encontrado", 401);
+  }
+  await boletimRepository.update(id, { finalizado: data.finalizado });
+  const boletim = await boletimRepository.findOneBy({ id });
+  return boletim!;
+};
 
-const updateBulletinService = async (data:IBoletimUpdateRequest, id: string):Promise<Boletim> => {
-    const updateSerializer = await updateBulletinSerializer.validate(data);
-    const boletimRepository = AppDataSource.getRepository(Boletim);
-    const findBoletim = await boletimRepository.findOneBy({id});
-    if(!findBoletim) {
-        throw new AppError("Boletim de ocorrencia não encontrado", 401)
-    }
-    await boletimRepository.update(id,{finalizado: data.finalizado});
-    const boletim = await boletimRepository.findOneBy({id})
-    return boletim
-}
-
-export default updateBulletinService
+export default updateBulletinService;
