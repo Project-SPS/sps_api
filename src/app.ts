5bb49d9474e2ddac780e436e969988eb77565009
Vitor Brasil <vitorlbrasil@hotmail.com>
2022-11-04 19:34:13 -0300

    refactor: refactoring app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
-import boletimRouter from "./routes/boletim.routes";
 import { appRoutes } from "./routes";
 import { handleErrorMiddleware } from "./middlewares";
 
 const app = express();
 app.use(express.json());
-
-app.use("/boletim", boletimRouter);
 
 appRoutes(app);
 
