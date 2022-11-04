b9b3bc9c4c63e173cd763e16806053b2a301343d
nicoleantuness <nicoleantunes2016@gmail.com>
2022-11-04 08:30:30 -0300

    feat: create fugitive controller and services

---
+++ src/services/produrados/createFugitive.services.ts
@@ -0,0 +1,30 @@
+import { AppDataSource } from "../../data-source";
+import { IProcurados } from "../../interfaces/procurados.interfaces";
+import { Procurado } from "../../entity/Procurado.entity";
+import { Cidadao } from "../../entity/Cidadao.entity";
+import { AppError } from "../../errors/AppError";
+const createFugitiveServices = async (fugitive: IProcurados) => {
+  const fugitivesRepository = AppDataSource.getRepository(Procurado);
+  const citiziensRepository = AppDataSource.getRepository(Cidadao);
+
+  const getCitiezen = await citiziensRepository.findOneBy({
+    id: fugitive.cidadao_id,
+  });
+
+  if (!getCitiezen) {
+    throw new AppError("Citizen not found", 404);
+  }
+
+  const newFugitive = new Procurado();
+
+  newFugitive.descricao = fugitive.descricao;
+  newFugitive.esta_ativo = fugitive.esta_ativo;
+
+  fugitivesRepository.create(newFugitive);
+
+  await fugitivesRepository.save(newFugitive);
+
+  return fugitive;
+};
+
+export default createFugitiveServices;
